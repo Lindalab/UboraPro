a605853da50135d939401186ba725dd26f68c939
Lindalab <linda.arthur@ashesi.edu.gh>
2022-11-02 14:35:03 +0000

    First Commit

---
+++ settings/db_class.php
@@ -0,0 +1,66 @@
+<?php
+//database
+
+//database credentials
+require('db_cred.php');
+
+/**
+ *@author David Sampah
+ *@version 1.1
+ */
+class db_connection
+{
+	//properties
+	
+
+	//connect
+	/**
+	*Database connection
+	*@return bolean
+	**/
+	
+
+	//execute a query
+	/**
+	*Query the Database
+	*@param takes a connection and sql query
+	*@return bolean
+	**/
+	
+
+
+	//execute a query with mysqli real escape string
+	//to saveguard from sql injection
+	/**
+	*Query the Database
+	*@param takes a connection and sql query
+	*@return bolean
+	**/
+	
+
+
+	//fetch a data
+	/**
+	*get select data
+	*@return a record
+	**/
+	
+
+
+	//fetch all data
+	/**
+	*get select data
+	*@return all record
+	**/
+	
+
+
+	//count data
+	/**
+	*get select data
+	*@return a count
+	**/
+	
+	
+}
+?>
