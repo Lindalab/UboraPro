4235334198de2fdc6438fbcd1c26d7b82a93bf48
Lindalab <linda.arthur@ashesi.edu.gh>
2022-11-02 16:59:32 +0000

    sql

---
+++ uborapro_db.sql
@@ -0,0 +1,11 @@
+drop database if exists uborapo_db;
+create database uborapo_db;
+use uborapo_db;
+
+create table awards();
+create table user();
+create table order();
+create table tickets();
+create table payment();
+create table items(); or create table food();/ create table transportation();
+create table seat();
