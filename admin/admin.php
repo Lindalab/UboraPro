d22f3c04b367540c45da443f7c30c615189ba144
Cdelightt <95875320+Cdelightt@users.noreply.github.com>
2022-11-17 20:17:14 +0000

    admin dashboard

---
+++ admin/admin.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="en"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Admin Dashboard">
+    <meta name="description" content="">
+    <title>admin</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="admin.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 4.21.12, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"logo": "images/ubora2.png"
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="admin">
+    <meta property="og:type" content="website">
+  </head>
+  <body class="u-body u-xl-mode" data-lang="en"><header class="u-clearfix u-header u-white u-header" id="sec-ef51"><div class="u-clearfix u-sheet u-sheet-1">
+        <a href="https://nicepage.com" class="u-image u-logo u-image-1" data-image-width="1920" data-image-height="1080">
+          <img src="images/ubora2.png" class="u-logo-image u-logo-image-1">
+        </a>
+        <nav class="u-menu u-menu-one-level u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg class="u-svg-link" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+              <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</g></svg>
+            </a>
+          </div>
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="index.php" style="padding: 10px 20px;">View as User</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#" target="_blank" style="padding: 10px 20px;">Tickets</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#" target="_blank" style="padding: 10px 20px;">Voting</a>
+</li></ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="index.php">View as User</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+          <div class="u-countdown-message u-hidden">
+            <p>Sorry, the time is up.</p>
+          </div>
+        </div>
+      </div></header>
+    <section class="u-clearfix u-section-1" id="sec-8670">
+      <a href="" class="u-border-none u-btn u-button-style u-hover-palette-2-base u-btn-1"><span class="u-icon"><svg class="u-svg-content" viewBox="0 0 512 512" x="0px" y="0px" style="width: 1em; height: 1em;"><path d="m19.98,200.65909l180.67121,0l55.82879,-171.63684l55.82883,171.63684l180.67118,0l-146.16581,106.07619l55.83168,171.63684l-146.16587,-106.07908l-146.16584,106.07908l55.8317,-171.63684l-146.16586,-106.07619z"></path></svg><img></span>Remove User from System
+      </a>
+      <h1 class="u-text u-text-default u-text-1">Admin Dashboard</h1>
+      <div class="u-list u-list-1">
+        <div class="u-repeater u-repeater-1">
+          <div class="u-border-2 u-border-black u-container-style u-list-item u-repeater-item">
+            <div class="u-container-layout u-similar-container u-valign-middle u-container-layout-1">
+              <a href="Admin-Categories.html" class="u-border-2 u-border-palette-2-base u-btn u-btn-round u-button-style u-hover-palette-2-base u-none u-radius-6 u-text-body-color u-text-hover-white u-btn-2">Create Award Category</a>
+            </div>
+          </div>
+          <div class="u-border-2 u-border-black u-container-style u-list-item u-repeater-item">
+            <div class="u-container-layout u-similar-container u-valign-middle u-container-layout-2">
+              <a href="Admin-Categories.html" class="u-border-2 u-border-palette-2-base u-btn u-btn-round u-button-style u-hover-palette-2-base u-none u-radius-6 u-text-body-color u-text-hover-white u-btn-3">Edit Award Category</a>
+            </div>
+          </div>
+          <div class="u-border-2 u-border-black u-container-style u-list-item u-repeater-item">
+            <div class="u-container-layout u-similar-container u-valign-middle u-container-layout-3">
+              <a href="Admin-Categories.html" class="u-border-2 u-border-palette-2-base u-btn u-btn-round u-button-style u-hover-palette-2-base u-none u-radius-6 u-text-body-color u-text-hover-white u-btn-4">Delete Award Category</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="u-list u-list-2">
+        <div class="u-repeater u-repeater-2">
+          <div class="u-border-2 u-border-black u-container-style u-list-item u-repeater-item">
+            <div class="u-container-layout u-similar-container u-valign-bottom u-container-layout-4">
+              <a href="admin-Nominee.html" class="u-border-2 u-border-palette-2-base u-btn u-btn-round u-button-style u-hover-palette-2-base u-none u-radius-6 u-text-body-color u-text-hover-white u-btn-5">Create Award Nominee</a>
+            </div>
+          </div>
+          <div class="u-border-2 u-border-black u-container-style u-list-item u-repeater-item">
+            <div class="u-container-layout u-similar-container u-valign-bottom u-container-layout-5">
+              <a href="admin-Nominee.html" class="u-border-2 u-border-palette-2-base u-btn u-btn-round u-button-style u-hover-palette-2-base u-none u-radius-6 u-text-body-color u-text-hover-white u-btn-6">Edit&nbsp;Award Nominee</a>
+            </div>
+          </div>
+          <div class="u-border-2 u-border-black u-container-style u-list-item u-repeater-item">
+            <div class="u-container-layout u-similar-container u-valign-bottom u-container-layout-6">
+              <a href="admin-Nominee.html" class="u-border-2 u-border-palette-2-base u-btn u-btn-round u-button-style u-hover-palette-2-base u-none u-radius-6 u-text-body-color u-text-hover-white u-btn-7">Delete&nbsp;Award Nominee</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="u-shape u-shape-svg u-text-palette-2-dark-1 u-shape-1">
+        <svg class="u-svg-link" preserveAspectRatio="none" viewBox="0 0 160 160" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-f6d0"></use></svg>
+        <svg class="u-svg-content" viewBox="0 0 160 160" x="0px" y="0px" id="svg-f6d0"><path d="M14.5,7.3c0,4-3.3,7.3-7.3,7.3S0,11.2,0,7.3S3.3,0,7.3,0S14.5,3.3,14.5,7.3z M7.3,24.2c-4,0-7.3,3.3-7.3,7.3
+	s3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3S11.2,24.2,7.3,24.2z M31.5,0c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3S35.5,0,31.5,0
+	z M55.8,0c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3S63,11.2,63,7.3S59.7,0,55.8,0z M80,0c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3
+	s7.3-3.3,7.3-7.3S84,0,80,0z M104.2,0c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3c4,0,7.3-3.3,7.3-7.3S108.2,0,104.2,0z M128.5,0
+	c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3c4,0,7.3-3.3,7.3-7.3S132.5,0,128.5,0z M152.7,0c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3
+	c4,0,7.3-3.3,7.3-7.3S156.7,0,152.7,0z M31.5,24.2c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3S35.5,24.2,31.5,24.2z
+	 M55.8,24.2c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3S59.7,24.2,55.8,24.2z M80,24.2c-4,0-7.3,3.3-7.3,7.3
+	s3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3S84,24.2,80,24.2z M104.2,24.2c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3c4,0,7.3-3.3,7.3-7.3
+	S108.2,24.2,104.2,24.2z M128.5,24.2c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3c4,0,7.3-3.3,7.3-7.3S132.5,24.2,128.5,24.2z M7.3,48.5
+	c-4,0-7.3,3.3-7.3,7.3S3.3,63,7.3,63s7.3-3.3,7.3-7.3S11.2,48.5,7.3,48.5z M31.5,48.5c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3
+	s7.3-3.3,7.3-7.3S35.5,48.5,31.5,48.5z M55.8,48.5c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3S59.7,48.5,55.8,48.5z
+	 M80,48.5c-4,0-7.3,3.3-7.3,7.3S76,63,80,63s7.3-3.3,7.3-7.3S84,48.5,80,48.5z M104.2,48.5c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3
+	c4,0,7.3-3.3,7.3-7.3S108.2,48.5,104.2,48.5z M7.3,72.7C3.3,72.7,0,76,0,80s3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3S11.2,72.7,7.3,72.7z
+	 M31.5,72.7c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3S35.5,72.7,31.5,72.7z M55.8,72.7c-4,0-7.3,3.3-7.3,7.3
+	s3.3,7.3,7.3,7.3S63,84,63,80S59.7,72.7,55.8,72.7z M80,72.7c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3S84,72.7,80,72.7
+	z M7.3,97c-4,0-7.3,3.3-7.3,7.3c0,4,3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3C14.5,100.3,11.2,97,7.3,97z M31.5,97c-4,0-7.3,3.3-7.3,7.3
+	c0,4,3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3C38.8,100.3,35.5,97,31.5,97z M55.8,97c-4,0-7.3,3.3-7.3,7.3c0,4,3.3,7.3,7.3,7.3
+	s7.3-3.3,7.3-7.3C63,100.3,59.7,97,55.8,97z M7.3,121.2c-4,0-7.3,3.3-7.3,7.3c0,4,3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3
+	C14.5,124.5,11.2,121.2,7.3,121.2z M31.5,121.2c-4,0-7.3,3.3-7.3,7.3c0,4,3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3
+	C38.8,124.5,35.5,121.2,31.5,121.2z M7.3,145.5c-4,0-7.3,3.3-7.3,7.3c0,4,3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3
+	C14.5,148.8,11.2,145.5,7.3,145.5z"></path></svg>
+      </div>
+      <div class="u-align-left u-list u-list-3">
+        <div class="u-repeater u-repeater-3">
+          <div class="u-border-2 u-border-black u-container-style u-list-item u-repeater-item">
+            <div class="u-container-layout u-similar-container u-container-layout-7">
+              <a href="Admin-Transport.html" class="u-border-2 u-border-palette-2-base u-btn u-btn-round u-button-style u-hover-palette-2-base u-none u-radius-6 u-text-body-color u-text-hover-white u-btn-8">Create Available Transport</a>
+            </div>
+          </div>
+          <div class="u-border-2 u-border-black u-container-style u-list-item u-repeater-item">
+            <div class="u-container-layout u-similar-container u-container-layout-8">
+              <a href="Admin-Transport.html" class="u-border-2 u-border-palette-2-base u-btn u-btn-round u-button-style u-hover-palette-2-base u-none u-radius-6 u-text-body-color u-text-hover-white u-btn-9">Edit Available Transport</a>
+            </div>
+          </div>
+          <div class="u-border-2 u-border-black u-container-style u-list-item u-repeater-item">
+            <div class="u-container-layout u-similar-container u-container-layout-9">
+              <a href="Admin-Transport.html" class="u-border-2 u-border-palette-2-base u-btn u-btn-round u-button-style u-hover-palette-2-base u-none u-radius-6 u-text-body-color u-text-hover-white u-btn-10">Delete Available Transport</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="u-list u-list-4">
+        <div class="u-repeater u-repeater-4">
+          <div class="u-border-2 u-border-black u-container-style u-list-item u-repeater-item">
+            <div class="u-container-layout u-similar-container u-valign-middle u-container-layout-10">
+              <a href="Admin-Food.html" class="u-border-2 u-border-palette-2-base u-btn u-btn-round u-button-style u-hover-palette-2-base u-none u-radius-6 u-text-body-color u-text-hover-white u-btn-11">Create Food Package</a>
+            </div>
+          </div>
+          <div class="u-border-2 u-border-black u-container-style u-list-item u-repeater-item">
+            <div class="u-container-layout u-similar-container u-valign-middle u-container-layout-11">
+              <a href="Admin-Food.html" class="u-border-2 u-border-palette-2-base u-btn u-btn-round u-button-style u-hover-palette-2-base u-none u-radius-6 u-text-body-color u-text-hover-white u-btn-12">Edit Food Package</a>
+            </div>
+          </div>
+          <div class="u-border-2 u-border-black u-container-style u-list-item u-repeater-item">
+            <div class="u-container-layout u-similar-container u-valign-middle u-container-layout-12">
+              <a href="Admin-Food.html" class="u-border-2 u-border-palette-2-base u-btn u-btn-round u-button-style u-hover-palette-2-base u-none u-radius-6 u-text-body-color u-text-hover-white u-btn-13">Delete Food Package</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="u-rotation-parent u-rotation-parent-1">
+        <div class="u-rotate-180 u-shape u-shape-svg u-text-palette-2-dark-1 u-shape-2">
+          <svg class="u-svg-link" preserveAspectRatio="none" viewBox="0 0 160 160" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-cfc1"></use></svg>
+          <svg class="u-svg-content" viewBox="0 0 160 160" x="0px" y="0px" id="svg-cfc1"><path d="M14.5,7.3c0,4-3.3,7.3-7.3,7.3S0,11.2,0,7.3S3.3,0,7.3,0S14.5,3.3,14.5,7.3z M7.3,24.2c-4,0-7.3,3.3-7.3,7.3
+	s3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3S11.2,24.2,7.3,24.2z M31.5,0c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3S35.5,0,31.5,0
+	z M55.8,0c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3S63,11.2,63,7.3S59.7,0,55.8,0z M80,0c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3
+	s7.3-3.3,7.3-7.3S84,0,80,0z M104.2,0c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3c4,0,7.3-3.3,7.3-7.3S108.2,0,104.2,0z M128.5,0
+	c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3c4,0,7.3-3.3,7.3-7.3S132.5,0,128.5,0z M152.7,0c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3
+	c4,0,7.3-3.3,7.3-7.3S156.7,0,152.7,0z M31.5,24.2c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3S35.5,24.2,31.5,24.2z
+	 M55.8,24.2c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3S59.7,24.2,55.8,24.2z M80,24.2c-4,0-7.3,3.3-7.3,7.3
+	s3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3S84,24.2,80,24.2z M104.2,24.2c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3c4,0,7.3-3.3,7.3-7.3
+	S108.2,24.2,104.2,24.2z M128.5,24.2c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3c4,0,7.3-3.3,7.3-7.3S132.5,24.2,128.5,24.2z M7.3,48.5
+	c-4,0-7.3,3.3-7.3,7.3S3.3,63,7.3,63s7.3-3.3,7.3-7.3S11.2,48.5,7.3,48.5z M31.5,48.5c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3
+	s7.3-3.3,7.3-7.3S35.5,48.5,31.5,48.5z M55.8,48.5c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3S59.7,48.5,55.8,48.5z
+	 M80,48.5c-4,0-7.3,3.3-7.3,7.3S76,63,80,63s7.3-3.3,7.3-7.3S84,48.5,80,48.5z M104.2,48.5c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3
+	c4,0,7.3-3.3,7.3-7.3S108.2,48.5,104.2,48.5z M7.3,72.7C3.3,72.7,0,76,0,80s3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3S11.2,72.7,7.3,72.7z
+	 M31.5,72.7c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3S35.5,72.7,31.5,72.7z M55.8,72.7c-4,0-7.3,3.3-7.3,7.3
+	s3.3,7.3,7.3,7.3S63,84,63,80S59.7,72.7,55.8,72.7z M80,72.7c-4,0-7.3,3.3-7.3,7.3s3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3S84,72.7,80,72.7
+	z M7.3,97c-4,0-7.3,3.3-7.3,7.3c0,4,3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3C14.5,100.3,11.2,97,7.3,97z M31.5,97c-4,0-7.3,3.3-7.3,7.3
+	c0,4,3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3C38.8,100.3,35.5,97,31.5,97z M55.8,97c-4,0-7.3,3.3-7.3,7.3c0,4,3.3,7.3,7.3,7.3
+	s7.3-3.3,7.3-7.3C63,100.3,59.7,97,55.8,97z M7.3,121.2c-4,0-7.3,3.3-7.3,7.3c0,4,3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3
+	C14.5,124.5,11.2,121.2,7.3,121.2z M31.5,121.2c-4,0-7.3,3.3-7.3,7.3c0,4,3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3
+	C38.8,124.5,35.5,121.2,31.5,121.2z M7.3,145.5c-4,0-7.3,3.3-7.3,7.3c0,4,3.3,7.3,7.3,7.3s7.3-3.3,7.3-7.3
+	C14.5,148.8,11.2,145.5,7.3,145.5z"></path></svg>
+        </div>
+      </div>
+    </section>
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-image u-footer" id="sec-ff33" data-image-width="1280" data-image-height="941"><div class="u-clearfix u-sheet u-valign-top-lg u-valign-top-xl u-sheet-1">
+        <p class="u-small-text u-text u-text-body-color u-text-variant u-text-1">(c) DELC&nbsp; All Rights reserved</p>
+      </div></footer>
+    <section class="u-backlink u-clearfix u-grey-80">
+      <a class="u-link" href="https://nicepage.com/templates" target="_blank">
+        <span>Free Templates</span>
+      </a>
+      <p class="u-text">
+        <span>created with</span>
+      </p>
+      <a class="u-link" href="" target="_blank">
+        <span>Website Builder Software</span>
+      </a>. 
+    </section>
+  
+</body></html>
