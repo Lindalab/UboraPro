8877e4a1a3d804afa7b2d3810377629716889c7a
Eric Gadzi <78027399+Eric-Gadzi@users.noreply.github.com>
2022-11-17 00:00:09 +0000

    award action

--- actions/awards_action/update_award.php
+++ actions/awards_action/update_award.php
@@ -4,16 +4,19 @@
     $award_id = $_GET['award_id'];
     $award_name = $_GET['award_name'];
     $award_description = $_GET['award_description'];
-    
+
     $award_image = $_GET['award_image'];
+
+    $user_file_name = $_FILES['award_image']['name'];
+    $target_dir = "../images/nominees/";
+    $award_image = $target_dir.$user_file_name;
+    move_uploaded_file($_FILES["award_image"]['tmp_name'], $award_image);
 
     $result = updateAward_ctr($award_id, $award_name, $award_description, $award_image);
 
     if($result){
-        
         echo "success";
-    }else{
-        
+    }else{ 
         echo "failed";
     }
 ?>
