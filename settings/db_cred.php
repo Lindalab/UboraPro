c35bccf66892d258d88678197628af850b3373a6
Thatguycalledcoder <quarshiedavid62@gmail.com>
2022-11-20 23:21:56 +0000

    db changes

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,9 +1,14 @@
 <?php
 //Database credentials
-define("SERVERNAME","localhost");
-define("HOSTNAME","root");
-define("PASSWORD","");
-define("DATABASE","uborapro");
+// define("SERVERNAME","localhost");
+// define("HOSTNAME","root");
+// define("PASSWORD","");
+// define("DATABASE","uborapro");
 
-
+// mysql://b8a8abf369b61d:e1ff8c71@us-cdbr-east-06.cleardb.net/heroku_987ce20d99f8b12?reconnect=true
+// Heroku credentials
+define("SERVERNAME","us-cdbr-east-06.cleardb.net");
+define("HOSTNAME","b8a8abf369b61d");
+define("PASSWORD","e1ff8c71");
+define("DATABASE","heroku_987ce20d99f8b12");
 ?>
