0be5d144c29cf8b3e2ffa35f1956bb883c0cdac2
Cdelightt <95875320+Cdelightt@users.noreply.github.com>
2022-11-20 16:39:51 +0000

    payment frontend

---
+++ view/Paymentfrontend.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="en"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <title>Payment</title>
+    <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="Payment.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 4.21.12, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    
+    
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"logo": "images/ubora2.png"
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="Payment">
+    <meta property="og:type" content="website">
+  </head>
+  <body class="u-body u-xl-mode" data-lang="en"><header class="u-clearfix u-header u-white u-header" id="sec-ef51"><div class="u-clearfix u-sheet u-sheet-1">
+        <a href="https://nicepage.com" class="u-image u-logo u-image-1" data-image-width="1920" data-image-height="1080">
+          <img src="images/ubora2.png" class="u-logo-image u-logo-image-1">
+        </a>
+        <nav class="u-menu u-menu-one-level u-offcanvas u-menu-1">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg class="u-svg-link" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+              <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</g></svg>
+            </a>
+          </div>
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#" style="padding: 10px 20px;">View as User</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#" target="_blank" style="padding: 10px 20px;">Tickets</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#" target="_blank" style="padding: 10px 20px;">Voting</a>
+</li></ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="#">View as User</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="#" target="_blank">Tickets</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link" href="#" target="_blank">Voting</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+        <div class="u-countdown u-countdown-1" data-timer-id="6bac" data-type="to-date" data-target-date="Tue, 22 Nov 2022 04:39:48 GMT" data-for="everyone" data-direction="down" data-time-left="750m" data-target-number="100" data-start-time="Mon Nov 14 2022 16:12:21 GMT+0000 (Greenwich Standard Time)" data-frequency="1s" data-after-count="none" data-redirect-url="https://">
+          <div class="u-countdown-wrapper u-spacing-20">
+            <div class="u-countdown-item u-countdown-years u-hidden u-spacing-10">
+              <div class="u-countdown-counter u-countdown-counter-1"><div class="u-countdown-number">0</div><div class="u-countdown-number u-hidden">0</div></div>
+              <div class="u-countdown-label u-countdown-label-1">Years</div>
+            </div>
+            <div class="u-countdown-separator u-hidden u-countdown-separator-1">:</div>
+            <div class="u-countdown-days u-countdown-item u-spacing-10">
+              <div class="u-countdown-counter u-countdown-counter-2"><div class="u-countdown-number">7</div><div class="u-countdown-number u-hidden">6</div><div class="u-countdown-number u-hidden">0</div></div>
+              <div class="u-countdown-label u-countdown-label-2">Days</div>
+            </div>
+            <div class="u-countdown-separator u-countdown-separator-2">:</div>
+            <div class="u-countdown-hours u-countdown-item u-spacing-10">
+              <div class="u-countdown-counter u-countdown-counter-3"><div class="u-countdown-number">1</div><div class="u-countdown-number">2</div></div>
+              <div class="u-countdown-label u-countdown-label-3">Hours</div>
+            </div>
+            <div class="u-countdown-separator u-countdown-separator-3">:</div>
+            <div class="u-countdown-item u-countdown-minutes u-spacing-10">
+              <div class="u-countdown-counter u-countdown-counter-4"><div class="u-countdown-number">3</div><div class="u-countdown-number">0</div></div>
+              <div class="u-countdown-label u-countdown-label-4">Minutes</div>
+            </div>
+            <div class="u-countdown-separator u-countdown-separator-4">:</div>
+            <div class="u-countdown-item u-countdown-seconds u-spacing-10">
+              <div class="u-countdown-counter u-countdown-counter-5"><div class="u-countdown-number">0</div><div class="u-countdown-number">0</div></div>
+              <div class="u-countdown-label u-countdown-label-5">Seconds</div>
+            </div>
+            <div class="u-countdown-separator u-hidden u-countdown-separator-5">:</div>
+            <div class="u-countdown-item u-countdown-numbers u-hidden u-spacing-10">
+              <div class="u-countdown-counter u-countdown-counter-6"><div class="u-countdown-number">0</div><div class="u-countdown-number">0</div></div>
+              <div class="u-countdown-label u-countdown-label-6">Items</div>
+            </div>
+          </div>
+          <div class="u-countdown-message u-hidden">
+            <p>Sorry, the time is up.</p>
+          </div>
+        </div>
+      </div></header>
+    <section class="u-clearfix u-section-1" id="sec-94e1">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <div class="u-align-center u-form u-form-1">
+          <form action="https://forms.nicepagesrv.com/Form/Process" class="u-clearfix u-form-spacing-33 u-form-vertical u-inner-form" source="email" name="form" style="padding: 50px;">
+            <div class="u-form-group u-form-name">
+              <label for="name-a85c" class="u-label">Paid Amount</label>
+              <input type="text" placeholder="Amount" id="name-a85c" name="amount" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="">
+            </div>
+            <div class="u-form-checkbox-group u-form-group u-form-input-layout-horizontal u-form-group-2">
+              <label class="u-label">Payment Currency</label>
+              <div class="u-form-checkbox-group-wrapper">
+                <div class="u-input-row">
+                  <input id="field-b372" type="checkbox" value="USD" name="checkbox[]">
+                  <label class="u-label" for="field-b372">USD</label>
+                </div>
+                <div class="u-input-row">
+                  <input id="field-a44a" type="checkbox" value="GHS" name="checkbox[]">
+                  <label class="u-label" for="field-a44a">GHS</label>
+                </div>
+                <div class="u-input-row">
+                  <input id="field-fad4" type="checkbox" value="EUR" name="checkbox[]">
+                  <label class="u-label" for="field-fad4">EUR</label>
+                </div>
+                <div class="u-input-row">
+                  <input id="field-7108" type="checkbox" value="YEN" name="checkbox[]">
+                  <label class="u-label" for="field-7108">YEN</label>
+                </div>
+              </div>
+            </div>
+            <div class="u-form-date u-form-group u-form-group-3">
+              <label for="date-4a1f" class="u-label">Payment Date</label>
+              <input type="date" placeholder="MM/DD/YYYY" id="date-4a1f" name="date" class="u-border-1 u-border-grey-30 u-input u-input-rectangle u-white" required="">
+            </div>
+            <div class="u-form-checkbox-group u-form-group u-form-group-4">
+              <label class="u-label">Payment option</label>
+              <div class="u-form-checkbox-group-wrapper">
+                <div class="u-input-row">
+                  <input id="field-ab49" type="checkbox" value="Visa" name="checkbox-1[]">
+                  <label class="u-label" for="field-ab49">Visa</label>
+                </div>
+                <div class="u-input-row">
+                  <input id="field-7068" type="checkbox" value="Mobile Money" name="checkbox-1[]">
+                  <label class="u-label" for="field-7068">Mobile Money</label>
+                </div>
+              </div>
+            </div>
+            <div class="u-align-center u-form-group u-form-submit">
+              <a href="#" class="u-btn u-btn-submit u-button-style">Make Payment</a>
+              <input type="submit" value="submit" class="u-form-control-hidden">
+            </div>
+            <div class="u-form-send-message u-form-send-success"> Thank you! Your message has been sent. </div>
+            <div class="u-form-send-error u-form-send-message"> Unable to send your message. Please fix errors then try again. </div>
+            <input type="hidden" value="" name="recaptchaResponse">
+            <input type="hidden" name="formServices" value="d544b4c863795dc104fe754b85fafb8a">
+          </form>
+        </div>
+        <img class="u-image u-image-contain u-image-default u-image-1" src="images/ubora2.png" alt="" data-image-width="1920" data-image-height="1080">
+      </div>
+    </section>
+    
+    
+    <footer class="u-align-center u-clearfix u-footer u-image u-footer" id="sec-ff33" data-image-width="1280" data-image-height="941"><div class="u-clearfix u-sheet u-valign-top-lg u-valign-top-xl u-sheet-1">
+        <p class="u-small-text u-text u-text-body-color u-text-variant u-text-1">(c) DELC&nbsp; All Rights reserved</p>
+      </div></footer>
+    <section class="u-backlink u-clearfix u-grey-80">
+      <a class="u-link" href="https://nicepage.com/website-mockup" target="_blank">
+        <span>Website Mockup</span>
+      </a>
+      <p class="u-text">
+        <span>created with</span>
+      </p>
+      <a class="u-link" href="https://nicepage.studio" target="_blank">
+        <span>Website Builder</span>
+      </a>. 
+    </section>
+  
+</body></html>
